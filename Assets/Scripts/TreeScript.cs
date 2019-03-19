28e789719138191a9ac04ed2bc36d6f87abe95e5
Agwebberley <44350096+Agwebberley@users.noreply.github.com>
2019-03-19 12:55:50 -0700

    Stuff

--- Assets/Scripts/TreeScript.cs
+++ Assets/Scripts/TreeScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script : MonoBehaviour
+public class TreeScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
